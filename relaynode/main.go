4af32269e0fb84a08ec132dcab657c564d6e7f78
Chandra179 <chandrafirst67@gmail.com>
2025-06-27 19:12:25 +0700

    update

--- relaynode/main.go
+++ relaynode/main.go
@@ -10,24 +10,31 @@ import (
 )
 
 func main() {
+	fmt.Println("aaaa")
 	relay1, err := libp2p.New()
+	fmt.Println("bbb")
 	if err != nil {
 		fmt.Printf("Failed to create relay1: %v", err)
 		return
 	}
+	fmt.Println("ccc")
 	_, err = relay.New(relay1)
+	fmt.Println("ddd")
 	if err != nil {
 		fmt.Printf("Failed to instantiate the relay: %v", err)
 		return
 	}
 
+	fmt.Println("eee")
 	relay1info := peer.AddrInfo{
 		ID:    relay1.ID(),
 		Addrs: relay1.Addrs(),
 	}
+	fmt.Println("fff")
 	fmt.Println(relay1info.ID)
 	for _, addr := range relay1info.Addrs {
 		fmt.Println(addr)
 	}
+	fmt.Println("ggg")
 	select {}
 }
