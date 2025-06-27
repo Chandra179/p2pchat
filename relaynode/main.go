b60d35db08f715086e9a683a2e471d1c2a96635f
Chandra179 <chandrafirst67@gmail.com>
2025-06-27 19:07:43 +0700

    add

--- relaynode/main.go
+++ relaynode/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
@@ -13,12 +12,12 @@ import (
 func main() {
 	relay1, err := libp2p.New()
 	if err != nil {
-		log.Printf("Failed to create relay1: %v", err)
+		fmt.Printf("Failed to create relay1: %v", err)
 		return
 	}
 	_, err = relay.New(relay1)
 	if err != nil {
-		log.Printf("Failed to instantiate the relay: %v", err)
+		fmt.Printf("Failed to instantiate the relay: %v", err)
 		return
 	}
 
