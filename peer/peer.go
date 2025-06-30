c8b37007948d32b6a8f0e557af488ff58d917ec7
Chandra <51901563+Chandra179@users.noreply.github.com>
2025-06-30 22:44:09 +0700

    feat: kademlia DHT (#3)
    dht, peer listen address

--- peer/peer.go
+++ peer/peer.go
@@ -8,12 +8,16 @@ import (
 	"p2p/config"
 	"p2p/utils"
 	"strings"
+	"time"
 
 	"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
-	autonat "github.com/libp2p/go-libp2p/p2p/host/autonat"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 	rhost "github.com/libp2p/go-libp2p/p2p/host/routed"
 	"github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/client"
 	ma "github.com/multiformats/go-multiaddr"
@@ -53,7 +57,6 @@ func (cl *ConnLogger) ClosedStream(net network.Network, stream network.Stream) {
 }
 
 func initPeer(cfg *config.Config) (*PeerInfo, error) {
-	// priv key is for relay ID
 	privKeyRelay, err := utils.DecodePrivateKey(cfg.RelayID)
 	if err != nil {
 		fmt.Printf("Failed to decode private key: %v\n", err)
@@ -64,16 +67,17 @@ func initPeer(cfg *config.Config) (*PeerInfo, error) {
 		log.Printf("Failed to derive relay ID from private key: %v", err)
 		return nil, err
 	}
-	privKeyPeer, err := utils.DecodePrivateKey(cfg.PeerID)
-	if err != nil {
-		fmt.Printf("Failed to decode private key: %v\n", err)
-		return nil, err
-	}
 	relayAddr, err := ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/%s", cfg.PublicIP, cfg.RelayPort))
 	if err != nil {
 		log.Printf("Failed to parse multiaddr: %v", err)
 		return nil, err
 	}
+	privKeyPeer, err := utils.DecodePrivateKey(cfg.PeerID)
+	if err != nil {
+		fmt.Printf("Failed to decode private key: %v\n", err)
+		return nil, err
+	}
+	listenAddr := fmt.Sprintf("/ip4/0.0.0.0/tcp/%s", cfg.PeerPort)
 	peerHost, err := libp2p.New(
 		libp2p.Identity(privKeyPeer),
 		libp2p.NoListenAddrs,
@@ -83,19 +87,70 @@ func initPeer(cfg *config.Config) (*PeerInfo, error) {
 		libp2p.DefaultMuxers,
 		libp2p.DefaultSecurity,
 		libp2p.NATPortMap(),
+		libp2p.ListenAddrStrings(listenAddr),
 	)
 	if err != nil {
 		log.Printf("Failed to create node: %v", err)
 		return nil, err
 	}
 
+	// Start a DHT, for use in peer discovery. We can't just make a new DHT
+	// client because we want each peer to maintain its own local copy of the
+	// DHT, so that the bootstrapping node of the DHT can go down without
+	// inhibiting future peer discovery.
+	ctx := context.Background()
+	bootstrapPeers := make([]peer.AddrInfo, len(dht.DefaultBootstrapPeers))
+	for i, addr := range dht.DefaultBootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(addr)
+		bootstrapPeers[i] = *peerinfo
+	}
+	kademliaDHT, err := dht.New(ctx, peerHost, dht.BootstrapPeers(bootstrapPeers...))
+	if err != nil {
+		panic(err)
+	}
+	// Bootstrap the DHT. In the default configuration, this spawns a Background
+	// thread that will refresh the peer table every five minutes.
+	fmt.Println("Bootstrapping the DHT")
+	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+		panic(err)
+	}
+
+	// Wait a bit to let bootstrapping finish (really bootstrap should block until it's ready, but that isn't the case yet.)
+	time.Sleep(1 * time.Second)
+
+	// We use a rendezvous point "meet me here" to announce our location.
+	// This is like telling your friends to meet you at the Eiffel Tower.
+	fmt.Println("Announcing ourselves...")
+	routingDiscovery := drouting.NewRoutingDiscovery(kademliaDHT)
+	dutil.Advertise(ctx, routingDiscovery, "/customprotocol")
+	fmt.Println("Successfully announced!")
+
+	// Now, look for others who have announced
+	// This is like your friend telling you the location to meet you.
+	fmt.Println("Searching for other peers...")
+	peerChan, err := routingDiscovery.FindPeers(ctx, "/customprotocol")
+	if err != nil {
+		panic(err)
+	}
+
+	for peer := range peerChan {
+		if peer.ID == peerHost.ID() {
+			fmt.Println("Found our own peer:", peer.ID)
+			continue
+		}
+		fmt.Println("Connecting to:", peer.ID)
+		_, err := peerHost.NewStream(ctx, peer.ID, protocol.ID("/customprotocol"))
+
+		if err != nil {
+			fmt.Println("Connection failed:", err)
+			continue
+		}
+		// TODO: we can send the peer ID to channel and list all the connected peers
+		// maybe we can display it in the UI
+	}
+
 	// Register Notifiee for live connection transitions
 	peerHost.Network().Notify(&ConnLogger{})
-	peerNat, err := autonat.New(peerHost)
-	if err != nil {
-		fmt.Println("AutoNAT failed:", err)
-	}
-	fmt.Println("AutoNAT status:", peerNat.Status())
 	peerHost.SetStreamHandler("/customprotocol", func(s network.Stream) {
 		buf := make([]byte, 1024)
 		n, err := s.Read(buf)
