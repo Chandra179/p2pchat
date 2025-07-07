0e1a911215deaebb9fe4ca6b2ed9f9e01d0ef86d
Chandra179 <chandrafirst67@gmail.com>
2025-07-07 18:09:23 +0700

    add

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"bufio"
 	"log"
 	"os"
 
@@ -9,12 +8,11 @@ import (
 )
 
 type Config struct {
-	PeerPort       string
-	RelayPort      string
-	RelayID        string
-	PeerID         string
-	RelayIP        string
-	BootstrapAddrs []string
+	PeerPort  string
+	RelayPort string
+	RelayID   string
+	PeerID    string
+	RelayIP   string
 }
 
 func LoadConfig() *Config {
@@ -22,31 +20,11 @@ func LoadConfig() *Config {
 		log.Printf("Failed to load .env: %v", err)
 	}
 
-	// Read bootstrap addresses from CSV
-	bootstrapAddrs := []string{}
-	file, err := os.Open("bootstrappeer.csv")
-	if err == nil {
-		defer file.Close()
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			addr := scanner.Text()
-			if addr != "" {
-				bootstrapAddrs = append(bootstrapAddrs, addr)
-			}
-		}
-		if err := scanner.Err(); err != nil {
-			log.Printf("Error reading bootstrappeer.csv: %v", err)
-		}
-	} else {
-		log.Printf("Could not open bootstrappeer.csv: %v", err)
-	}
-
 	return &Config{
-		PeerPort:       os.Getenv("PEER_TCP_PORT"),
-		RelayPort:      os.Getenv("RELAY_TCP_PORT"),
-		RelayID:        os.Getenv("RELAY_ID"),
-		PeerID:         os.Getenv("PEER_ID"),
-		RelayIP:        os.Getenv("RELAY_IP"),
-		BootstrapAddrs: bootstrapAddrs,
+		PeerPort:  os.Getenv("PEER_TCP_PORT"),
+		RelayPort: os.Getenv("RELAY_TCP_PORT"),
+		RelayID:   os.Getenv("RELAY_ID"),
+		PeerID:    os.Getenv("PEER_ID"),
+		RelayIP:   os.Getenv("RELAY_IP"),
 	}
 }
